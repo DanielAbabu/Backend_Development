f67d13cf4dd32e0c27ecc63f598153596da297ee
DanielAbabu <Daniel0Ababu@gmail.com>
2024-07-30 15:48:22 +0300

    task 2 done

--- Task 2/main.go
+++ Task 2/main.go
@@ -37,7 +37,7 @@ func Count(s string) map[string]int {
 	return dict
 }
 
-func Plaindrome(str string) bool {
+func Palindrome(str string) bool {
 	var newstr string = Letteronly(strings.ToLower(str))
 
 	var reversed string = ""
@@ -45,19 +45,55 @@ func Plaindrome(str string) bool {
 	for i := len(newstr) - 1; i >= 0; i-- {
 		reversed += string(newstr[i])
 	}
-	fmt.Println(newstr)
 	return reversed == newstr
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Println("Enter the String you want to count the word Frequency:")
-	str, _ := reader.ReadString('\n')
-	fmt.Println(Count(str))
+	for {
+		fmt.Println("\nChoose an option:")
+		fmt.Println("1. Count word frequency")
+		fmt.Println("2. Check if a string is a palindrome")
+		fmt.Println("3. Exit\n")
 
-	fmt.Println("Enter the String you want to check if it is Palindrom:")
-	strr, _ := reader.ReadString('\n')
-	fmt.Println(Plaindrome(strr))
+		option, _ := reader.ReadString('\n')
+		option = strings.TrimSpace(option)
+
+		switch option {
+
+		case "1":
+			fmt.Println("Enter the string you want to count the word frequency:")
+			str, _ := reader.ReadString('\n')
+			str = strings.TrimSpace(str)
+
+			wordcount := Count(str)
+
+			fmt.Println("\nWord frequency count:")
+			fmt.Println(strings.Repeat("-", 40))
+			fmt.Printf("%-20s %-10s\n", "Word", "Count")
+			fmt.Println(strings.Repeat("-", 40))
+			for word, count := range wordcount {
+				fmt.Printf("%-20s %-10.2d\n", word, count)
+			}
+
+		case "2":
+			fmt.Println("Enter the string you want to check if it is a palindrome:")
+			str, _ := reader.ReadString('\n')
+			str = strings.TrimSpace(str)
+			if Palindrome(str) {
+				fmt.Printf("\nThe '%s' is a palindrome.\n", str)
+			} else {
+				fmt.Printf("\nThe '%s' is not a palindrome.\n", str)
+			}
+
+		case "3":
+			fmt.Println("Exiting the program.")
+			return
+
+		default:
+			fmt.Println("Invalid option. Please choose again.")
+		}
+	}
 
 }
